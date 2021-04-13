9f45b249373dd4deaf585952657784627de7c244
AnushkaC2008 <79006208+AnushkaC2008@users.noreply.github.com>
2021-04-13 21:49:58 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,33 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.body=Bodies.cirle(x, y, r , options);
+        this.image = loadImage("paper.png");
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			imageMode(CENTER)
+			image(this.image, 0, 0, this.r);
+			pop()
+			
+	}
+
+}
